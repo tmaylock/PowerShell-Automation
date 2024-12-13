17c5f83c6bd9f03e28fbad3287598117bca337c7
Tyler Maylock <52976263+tmaylock@users.noreply.github.com>
2024-12-12 22:01:19 -0500

    Added shortcut for using $pwd to modify PSModulePath manually.

--- Scheduler.ps1
+++ Scheduler.ps1
@@ -9,5 +9,6 @@ param (
 $global:rootdir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 $Env:PSModulePath = $Env:PSModulePath+";$rootdir\Modules"
+#$Env:PSModulePath = $Env:PSModulePath+";$pwd\Modules"
 #Import-Module DataCollector.TM -Force
 #Invoke-DataCollection -Schedule $Schedule
